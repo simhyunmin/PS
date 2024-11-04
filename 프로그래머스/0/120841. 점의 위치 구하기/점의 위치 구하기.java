7d05c2a34694c797d7ff58cdb45b708216eff78c
simhyunmin <80190240+simhyunmin@users.noreply.github.com>
2024-11-04 10:07:33 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] dot) {
+        if(dot[0] * dot[1] >0){
+            if(dot[0] > 0){
+                return 1;
+            }
+            else{
+                return 3;
+            }
+        }
+        else if(dot[0] * dot[1] < 0){
+            if(dot[0] > 0 && dot[1] < 0){
+                return 4;
+            }
+            else if (dot[0] < 0 && dot[1] > 0){
+                return 2;
+            }       
+        }
+        return -1;
+    }
+}
