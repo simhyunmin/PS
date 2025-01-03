d95476c97197f0a4b920391688036e24c1c7f41e
simhyunmin <shm66371945@gmail.com>
2025-01-03 15:49:16 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 284 ms, Memory: 22160 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < T ; i++) {
+            for (int j = 0; j < T + i  ; j++) {
+                if (j < T - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
