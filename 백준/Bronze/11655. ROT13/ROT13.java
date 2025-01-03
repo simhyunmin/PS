1ca83cf52a58dd7435e5a9dc4fe031b9b9092327
simhyunmin <shm66371945@gmail.com>
2025-01-03 17:26:16 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -8,17 +8,14 @@ public class Main {
         String str = br.readLine();
         String newStr = "";
         for (Character c : str.toCharArray()) {
-            if (Character.isDigit(c)) {
-                newStr += String.valueOf(c);
-            } else if (c == ' ') {
-                newStr += String.valueOf(c);
-            } else {
                 if (c >= 'a' && c <= 'z') {
                     newStr += String.valueOf((char)((c - 'a' + 13) % 26 + 'a'));
                 } else if (c >= 'A' && c <= 'Z') {
                     newStr += String.valueOf((char)((c - 'A' + 13) % 26 + 'A'));
+                } else {
+                    newStr += String.valueOf(c);
                 }
-            }
+
         }
         System.out.println(newStr);
     }
