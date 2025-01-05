f5fb02c3c705603f6041a2559c6131dad88369d6
simhyunmin <shm66371945@gmail.com>
2025-01-05 18:11:25 +0900

    [Bronze II] Title: 문자열 분석, Time: 152 ms, Memory: 16504 KB -BaekjoonHub

---
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while ((str = br.readLine()) != null) {
+            int upperCase = 0;
+            int lowerCase = 0;
+            int numCase = 0;
+            int emptyCase = 0;
+            for (char c : str.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    upperCase++;
+                } else if (Character.isLowerCase(c)) {
+                    lowerCase++;
+                } else if (Character.isDigit(c)) {
+                    numCase++;
+                } else if (c == ' ') {
+                    emptyCase++;
+                }
+            }
+            System.out.println(lowerCase + " " + upperCase + " " + numCase + " " + emptyCase);
+        }
+    }
+}
