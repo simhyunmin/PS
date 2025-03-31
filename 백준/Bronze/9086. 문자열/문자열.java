820c1967875e2b80273f42efa86bf13e8ea246f4
SimHyeonmin <shm66371945@gmail.com>
2025-03-31 10:16:50 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 15932 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String str = br.readLine();
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+        }
+
+    }
+}
